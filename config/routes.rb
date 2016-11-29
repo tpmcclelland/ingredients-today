b3836a64c627546b48850873e40951ecbd011981
Tom McClelland <tom@mcclelland.me>
2016-11-29 15:29:43 -0500

    initial layout for rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'static/index'
-  root 'static#index'
+  get 'landing/index'
+  root 'landing#index'
+  get  '/driver' => 'driver#index'
+  get  '/cooker' => 'cooker#index'
 end
