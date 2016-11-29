a043ba78a0b599827cb2170fab48a5de8e997ae3
JoelBot <Joel.Lonneker@gmail.com>
2016-11-29 15:05:03 -0500

    components folder and initial template React component

---
+++ webpack/components/TemplateController.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Template extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    componentWillMount() {
+
+    }
+    componentDidMount() {
+
+    }
+
+    render() {
+
+    }
+}
+
+export default Template
